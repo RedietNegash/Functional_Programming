f386242249c100a6d762dd4626b976a39975fb82
RedietNegash <redietnegash9@gmail.com>
2025-06-27 23:44:22 +0300

    arity

---
+++ arity.js
@@ -0,0 +1,9 @@
+ 
+
+ 
+ 
+ // This function has an arity of 1. Also called unary
+ const stringify = x =>`Current number is ${x}`
+
+ // This function has an arity of 2. Also called binary
+ const sum = (x, y) => x + y
